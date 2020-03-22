bf904a33e717502640221569e71e86b22c99a839
Jesús Alejandro Atao <jealeat96@gmail.com>
2020-03-21 21:14:25 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,28 +1,41 @@
-// Requires = Importacion de librerias
+// Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
-
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
+
 // Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
-    if ( err ) throw err;
-    
+
+    if (err) throw err;
+
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
+
 });
 
+
 // Rutas
-app.get('/', (req, res) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente!'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 // Escuchar peticiones
 app.listen(3000, () => {
-    console.log('Express server corriendo en el puerto 3000: \x1b[32m%s\x1b[0m', 'online');
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
